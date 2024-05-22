dadeca6a097b763b2c86eac2c486ba3b845ae3cf
reactwindd <71582030+reactwindd@users.noreply.github.com>
2024-05-22 10:21:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,112 +1,18 @@
 import express from "express";
-import dotenv from "dotenv";
+import route from "./api/route";
+import "dotenv/config";
 
-dotenv.config();
-const router = express.Router();
-router.use(express.json());
+const app = express();
+app.use(express.json());
 
-router.get("/", (req, res) => {
-    res.json({
-        message: "Hello World!",
-    });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
 });
 
-router.get("/getid", async (req, res) => {
-    const data = await fetch("https://jombaca-api.jazro.com.my/api/users/me", {
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${process.env.TOKEN}`,
-            Origin: "https://ains.moe.gov.my",
-        },
-    });
+app.use("/api", route);
 
-    res.json((await data.json()).id);
+app.use("*", (req, res) => {
+    res.status(404).json({ error: "not found" });
 });
-
-router.get("/getBook", async (req, res) => {
-    const word = await fetch(
-        "https://random-word-api.herokuapp.com/word?length=6"
-    );
-    const data = await fetch(
-        `https://www.googleapis.com/books/v1/volumes/?q=${await word.text()}`
-    );
-    const user = await fetch("https://jombaca-api.jazro.com.my/api/users/me", {
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${process.env.TOKEN}`,
-            Origin: "https://ains.moe.gov.my",
-        },
-    });
-
-    let book = await data.json();
-    let userData = await user.json();
-
-    function formatDate(date) {
-        var d = new Date(date),
-            month = "" + (d.getMonth() + 1),
-            day = "" + d.getDate(),
-            year = d.getFullYear();
-
-        if (month.length < 2) month = "0" + month;
-        if (day.length < 2) day = "0" + day;
-
-        return [year, month, day].join("-");
-    }
-
-    function formatPublishedDate(date) {
-        return date.slice(0, 4);
-    }
-
-    res.json({
-        data: {
-            user: userData.id,
-            type: "book",
-            date: formatDate(Date.now()),
-            title: book.items[0].volumeInfo.title,
-            bookType: "physical",
-            category: "fiction",
-            noOfPage: book.items[0].volumeInfo.pageCount,
-            isbn: book.items[0].volumeInfo.industryIdentifiers[0].identifier
-                ? book.items[0].volumeInfo.industryIdentifiers[0].identifier
-                : "-",
-            author: book.items[0].volumeInfo.authors[0]
-                ? book.items[0].volumeInfo.authors[0]
-                : "-",
-            publisher: book.items[0].volumeInfo.publisher,
-            publishedYear: formatPublishedDate(
-                book.items[0].volumeInfo.publishedDate
-            ),
-            language: "en",
-            summary: book.items[0].volumeInfo.description,
-            review: "It's Really Good",
-            rating: 5,
-            reviewIsVideo: false,
-        },
-    });
-});
-
-// router.get("/createBook", async (req: Request, res: Response) => {
-//     const book = await fetch("http://localhost:3000/api/getBook");
-//     const data = await book.json();
-//     console.log(data.data);
-
-//     const result = await fetch(
-//         "https://jombaca-api.jazro.com.my/api/nilam-records",
-//         {
-//             method: "POST",
-//             headers: {
-//                 Authorization: `Bearer ${process.env.TOKEN}`,
-//                 Origin: "https://ains.moe.gov.my",
-//             },
-//             body: JSON.stringify({
-//                 data: {
-//                     ...data.data,
-//                 },
-//             }),
-//         }
-//     );
-
-//     res.json(result.json);
-// });
-
-export default router;
